2e7ffd7523d32129e74cddeeb5ae0b1b446cc674
nemkicsiaron <aronfszabo@gmail.com>
2023-04-07 21:17:45 +0200

    Fixed and updated

--- script.js
+++ script.js
@@ -1,95 +1,63 @@
-const gameContainer = document.querySelector('.game-container');
-const gridContainer = document.querySelector('.grid-container');
-const gameStatus = document.querySelector('.game-status');
-const restartButton = document.querySelector('.restart-button');
-const gridSquares = document.querySelectorAll('.grid-square');
+const cells = document.querySelectorAll('.cell');
+let currentPlayer = 'x';
 
-const X_CLASS = 'x';
-const O_CLASS = 'o';
-const WINNING_COMBINATIONS = [  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-];
+cells.forEach(cell => {
+  cell.addEventListener('click', placeMark);
+});
 
-let xTurn = true;
-
-startGame();
-
-restartButton.addEventListener('click', startGame);
-
-function startGame() {
-  xTurn = true;
-  gridSquares.forEach(square => {
-    square.classList.remove(X_CLASS);
-    square.classList.remove(O_CLASS);
-    square.textContent = "";
-    square.removeEventListener('click', handleClick);
-    square.addEventListener('click', handleClick, { once: true });
-  });
-  setBoardHoverClass();
-  gameStatus.innerText = '';
-  gameContainer.classList.remove(O_CLASS);
-  gameContainer.classList.add(X_CLASS);
+function placeMark(e) {
+  const cell = e.target;
+  if (cell.classList.contains('x') || cell.classList.contains('o')) {
+    return;
+  }
+  cell.classList.add(currentPlayer);
+  cell.textContent = currentPlayer;
+  checkWin();
+  currentPlayer = currentPlayer === 'x' ? 'o' : 'x';
 }
 
-function handleClick(event) {
-  const square = event.target;
-  const currentClass = xTurn ? X_CLASS : O_CLASS;
-  placeMark(square, currentClass);
-  if (checkWin(currentClass)) {
-    endGame(false);
-  } else if (isDraw()) {
-    endGame(true);
-  } else {
-    swapTurns();
-    setBoardHoverClass();
+function checkWin() {
+  const winningCombos = [    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  const cellsArr = Array.from(cells);
+  const currentPlayerCells = cellsArr.reduce((acc, cell, index) => {
+    if (cell.classList.contains(currentPlayer)) {
+      acc.push(index);
+    }
+    return acc;
+  }, []);
+  for (let combo of winningCombos) {
+    if (combo.every(index => currentPlayerCells.includes(index))) {
+      declareWinner();
+      break;
+    } else if (cellsArr.every(cell => cell.classList.contains('x') || cell.classList.contains('o'))) {
+      declareDraw();
+      break;
+    }
   }
 }
 
-function placeMark(square, currentPlayer) {
-  square.textContent = currentPlayer;
+function declareWinner() {
+  alert(`Congratulations! Player ${currentPlayer.toUpperCase()} wins!`);
+  resetBoard();
 }
 
-function swapTurns() {
-  xTurn = !xTurn;
+function declareDraw() {
+  alert('It\'s a draw!');
+  resetBoard();
 }
 
-function setBoardHoverClass() {
-  gridContainer.classList.remove(X_CLASS);
-  gridContainer.classList.remove(O_CLASS);
-  if (xTurn) {
-    gridContainer.classList.add(X_CLASS);
-  } else {
-    gridContainer.classList.add(O_CLASS);
-  }
-}
-
-function checkWin(currentClass) {
-  return WINNING_COMBINATIONS.some(combination => {
-    return combination.every(index => {
-      return gridSquares[index].classList.contains(currentClass);
-    });
+function resetBoard() {
+  cells.forEach(cell => {
+    cell.classList.remove('x');
+    cell.classList.remove('o');
   });
-}
-
-function isDraw() {
-  return [...gridSquares].every(square => {
-    return square.classList.contains(X_CLASS) || square.classList.contains(O_CLASS);
-  });
-}
-
-function endGame(draw) {
-  if (draw) {
-    gameStatus.innerText = 'Draw!';
-  } else {
-    gameStatus.innerText = `${xTurn ? "X's" : "O's"} Wins!`;
-  }
-  gridSquares.forEach(square => {
-    square.removeEventListener('click', handleClick);
-  });
+  currentPlayer = 'x';
 }
